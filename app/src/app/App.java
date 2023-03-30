a353f24a8a9e97e529b7dfb28c1148a69911edae
martin-perez1 <129412018+martin-perez1@users.noreply.github.com>
2023-03-30 15:17:58 +0000

    mi primer commit

--- app/src/app/App.java
+++ app/src/app/App.java
@@ -15,6 +15,7 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo martin");
     }
     
 }
