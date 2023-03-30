6f7c2d4feaf7ff48ae8ca8fe7e60574b75bccd86
diego-f1 <129410820+diego-f1@users.noreply.github.com>
2023-03-30 17:06:09 +0000

    menu y funciones

--- app/src/app/App.java
+++ app/src/app/App.java
@@ -11,35 +11,85 @@ import java.util.Scanner;
  * @author ADSI
  */
 public class App {
+    static Scanner leer = new Scanner(System.in);
+    static String job, name, lastname;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String options = "saludo";
-        Scanner leer = new Scanner(System.in);
-        System.out.println("ingrese una  accion");
-        options = leer.nextLine();
-        switch (options) {
-            case "saludo":
-                System.out.println("Hola mundo david");
-                // secuencia de sentencias.
-                break;
-            case "despedida":
-                System.out.println("chao martin");
-                // secuencia de sentencias.
-                break;
-            case "hablar":
-                System.out.println("como estas diego?");
-                // secuencia de sentencias.
-                break;
-            default:
-                System.out.println("Hola mundo");
+        int options;
+        boolean menu = true;
+
+        while (menu) {
+            System.out.println("Menu");
+            System.out.println("ACCIONES:");
+            System.out.println("1)administrativo");
+            System.out.println("2)operativo");
+            System.out.println("3)Salir");
+            System.out.println("tipo de empleado:");
+            options = leer.nextInt();
+            switch (options) {
+                case 1:
+                    information();
+                    break;
+                case 2:
+                    System.out.println("empleado de tipo operativo");
+                    break;
+                case 3:
+                    menu = false;
+                    break;
+                default:
+                    System.out.println("no existe ese cargo");
+            }
 
         }
 
-    
-       
     }
 
+    static void imprimir(String name, String lastname) {
+        System.out.println("*************************************************");
+        System.out.println("**************** VOLANTE DE PAGO ****************");
+        System.out.println("*************************************************");
+        System.out.println("Nombre: Ana Maria Jaimes González");
+        System.out.println("Cargo: Auxiliar Administrativo");
+        System.out.println("Horas Trabajadas (mes): 40");
+        System.out.println("Salario Bruto: $800, 000");
+        System.out.println("Horas Extras: 20");
+        System.out.println("Total pago Horas Extras: 3500, 000");
+        System.out.println("*************************************************");
+        System.out.println("*************** DESCUENTOS DE LEY ***************");
+        System.out.println("*************************************************");
+        System.out.println("Salud(4%): $52000");
+        System.out.println("Pensión: $52000");
+        System.out.println("ARL(0,522%): $6786");
+        System.out.println("Total Descuentos: $104000");
+        System.out.println("Total a pagar: $1,189,214");
+        System.out.println("*************************************************");
+        System.out.println("************ FIN DEL VOLANTE DE PAGO ************");
+        System.out.println("*************************************************");
+
+    }
+
+    static void information() {
+        System.out.println("nombre del trabajador" + '\n');
+        name = leer.nextLine();
+        System.out.println("apellido del trabajador" + '\n');
+        lastname = leer.nextLine();
+        System.out.println("titulo de trabajo" + '\n');
+        job = leer.nextLine();
+        imprimir("name", "lastname");
+    }
+
+    static int Salary() {
+        return 100;
+    }
+
+    static int ARL(String level) {
+        return 100;
+    }
+
+    static int hearthand(String level) {
+        return 100;
+    }
 }
